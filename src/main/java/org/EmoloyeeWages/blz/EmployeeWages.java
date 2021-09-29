754e95252ce2e50417f308bf853329a838cf7fab
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-09-29 09:58:38 +0530

    Added:welcomemessage

---
+++ src/main/java/org/EmoloyeeWages/blz/EmployeeWages.java
@@ -0,0 +1,11 @@
+package org.EmoloyeeWages.blz;
+
+
+public class EmployeeWages
+{
+    public static void main( String[] args )
+
+    {
+        System.out.println( "Welcome To Employee Wage Computation" );
+    }
+}
