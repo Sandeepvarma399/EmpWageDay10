6e39f1a9105064683a883398b5ea9fe6433b93c9
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-09-29 19:14:33 +0530

    Added:Daily Employee Wage

--- src/main/java/org/EmoloyeeWages/blz/EmployeeWages.java
+++ src/main/java/org/EmoloyeeWages/blz/EmployeeWages.java
@@ -5,11 +5,17 @@ public class EmployeeWages {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
         int IS_FULL_TIME = 1;
+        int wageFORHOUR = 20;
+        int numberOfHours = 8;
+
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == 1) {
-            System.out.println( " Employee Is Present");
+            System.out.println(" Employee Is Present");
         } else {
             System.out.println(" Employee Is Absent");
+        }
+        if (empCheck == 1) {
+            System.out.println("Daily EmployeeWage Is " + wageFORHOUR * numberOfHours);
         }
     }
 }
