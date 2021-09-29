ee3fedf0b4629f008b32616cacae443b1fb6ef5e
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-09-29 20:16:29 +0530

    Added:Part Time Employee

--- src/main/java/org/EmoloyeeWages/blz/EmployeeWages.java
+++ src/main/java/org/EmoloyeeWages/blz/EmployeeWages.java
@@ -5,17 +5,24 @@ public class EmployeeWages {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
         int IS_FULL_TIME = 1;
-        int wageFORHOUR = 20;
-        int numberOfHours = 8;
+        int IS_PART_TIME = 2;
+        int wageForHour = 20;
+        int fullTimeHours = 8;
+        int partTimeHours = 4;
 
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == 1) {
             System.out.println(" Employee Is Present");
+            System.out.println(" Employee is fulltime EMPLOYEE");
+            System.out.println("Daily EmployeeWage for fulltime employee Is " + wageForHour * fullTimeHours);
+        } else if (empCheck == 2) {
+            System.out.println(" Employee Is Present");
+            System.out.println(" Employee is Parttime EMPLOYEE");
+            System.out.println("Daily EmployeeWage for parttime employee Is " + wageForHour * partTimeHours);
         } else {
             System.out.println(" Employee Is Absent");
         }
-        if (empCheck == 1) {
-            System.out.println("Daily EmployeeWage Is " + wageFORHOUR * numberOfHours);
-        }
+
     }
 }
+
