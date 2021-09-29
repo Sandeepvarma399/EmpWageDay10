af9adda0ac8de96ac1dbb634a23baa5cffe8b424
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-09-29 20:45:31 +0530

    Added:swichcase

--- src/main/java/org/EmoloyeeWages/blz/EmployeeWages.java
+++ src/main/java/org/EmoloyeeWages/blz/EmployeeWages.java
@@ -6,21 +6,24 @@ public class EmployeeWages {
         System.out.println("Welcome To Employee Wage Computation");
         int IS_FULL_TIME = 1;
         int IS_PART_TIME = 2;
-        int wageForHour = 20;
+        int wagePerHour = 20;
         int fullTimeHours = 8;
         int partTimeHours = 4;
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == 1) {
-            System.out.println(" Employee Is Present");
-            System.out.println(" Employee is fulltime EMPLOYEE");
-            System.out.println("Daily EmployeeWage for fulltime employee Is " + wageForHour * fullTimeHours);
-        } else if (empCheck == 2) {
-            System.out.println(" Employee Is Present");
-            System.out.println(" Employee is Parttime EMPLOYEE");
-            System.out.println("Daily EmployeeWage for parttime employee Is " + wageForHour * partTimeHours);
-        } else {
-            System.out.println(" Employee Is Absent");
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case 1:
+                System.out.println(" Employee Is Present");
+                System.out.println(" Employee is fulltime EMPLOYEE");
+                System.out.println("Daily EmployeeWage for fulltime employee Is " + wagePerHour * fullTimeHours);
+                break;
+            case 2:
+                System.out.println(" Employee Is Present");
+                System.out.println(" Employee is Parttime EMPLOYEE");
+                System.out.println("Daily EmployeeWage for parttime employee Is " + wagePerHour * partTimeHours);
+                break;
+            default:
+                System.out.println(" Employee Is Absent");
         }
 
     }
