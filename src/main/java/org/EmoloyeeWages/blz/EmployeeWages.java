a3b6aa15568e6a96a54bdd706a33539db1c41cab
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-08 22:57:55 +0530

    Added:save wage for each company

--- src/main/java/org/EmoloyeeWages/blz/EmployeeWages.java
+++ src/main/java/org/EmoloyeeWages/blz/EmployeeWages.java
@@ -4,55 +4,68 @@ package org.EmoloyeeWages.blz;
 public class EmployeeWages {
     public static final int  IS_FULL_TIME = 1;
     public static final int  IS_PART_TIME = 2;
-    public static final int  WAGE_PER_HOUR = 20;
-    public static final int  WORKING_DAYS = 20;
-    public static final int  TOTAL_HOURS = 100;
+    private final String company;
+    private final int empRatePerHr;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+    private int totalEmpWage;
+
+    public EmployeeWages(String company, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth) {
+        this.company =company;
+        this.empRatePerHr = empRatePerHr;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
-        System.out.println("the employewage for TCS is"  );
-        EmployeeWage("Cognizant", 15,18,100);
-        System.out.println("the employewage for INFOSYSis"  );
-        EmployeeWage("Accenture", 20,20,90);
-        System.out.println("the employewage for BRIDGELABZis"  );
-        EmployeeWage("Bridgelabz", 20,20,90);
+        EmployeeWages company1 = new EmployeeWages("Epam",15,25,100);
+        EmployeeWages company2 = new EmployeeWages("tcs",13,15,80);
+        EmployeeWages company3 = new EmployeeWages("bridgelabz",10,18,90);
+        company1.employeeWage();
+        company3.employeeWage();
+        company3.employeeWage();
+
+
     }
-    public static void EmployeeWage(String company, int empRatePerHr, int maxWorkDays, int maxWorkHrs) {
+    public int  employeeWage() {
         int empWage = 0;
-        int totalEmpWage = 0 ;
+
         int empHrs = 0;
         int totalEmpHours = 0;
         int day = 1;
-        while (day <= WORKING_DAYS && totalEmpHours <= TOTAL_HOURS ) {
+        while (day <= numOfWorkingDays && totalEmpHours <= maxHoursPerMonth ) {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_FULL_TIME:
-                    System.out.println("day " + day);
+
                     empHrs = 8;
-                    System.out.println(" Employee Is Present");
-                    System.out.println(" Employee is fulltime EMPLOYEE");
+
                     break;
                 case IS_PART_TIME:
-                    System.out.println("day " + day);
+
                     empHrs = 4 ;
-                    System.out.println(" Employee Is Present");
-                    System.out.println(" Employee is Parttime EMPLOYEE");
+
                     break;
                 default:
-                    System.out.println("day " + day);
+
                     empHrs = 0;
-                    System.out.println(" Employee Is Absent");
+
+
+
             }
 
-            empWage = empHrs * WAGE_PER_HOUR;
+            empWage = empHrs * empRatePerHr;
             totalEmpWage = totalEmpWage + empWage;
             totalEmpHours = totalEmpHours + empHrs;
 
             System.out.println(" Emp Wage: " + empWage);
             day++;
         }
-        System.out.println("Total Hours: " +  totalEmpHours);
-        System.out.println("Total Emp Wage " + totalEmpWage);
+
+        System.out.println("Total Emp Wage " + company + " is " + totalEmpWage );
+        return totalEmpWage;
+
 
         }
     }
